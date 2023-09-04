fa2ede824d175687fe4118e592058b181cab369e
NurGeo <anzpro@gmail.com>
2023-09-04 09:04:26 +0500

    добавлен caller;

---
+++ src/app/caller.ts
@@ -0,0 +1,20 @@
+import { IdType, UuidType } from '../common/types';
+
+export type AnonymousUser = {
+  type: 'AnonymousUser',
+  requestID: UuidType,
+};
+
+export type DomainUser = {
+  type: 'DomainUser',
+  requestID: UuidType,
+  id: IdType,
+};
+
+export type ModuleCaller = {
+  type: 'ModuleCaller',
+  name: string,
+  user: AnonymousUser | DomainUser,
+};
+
+export type Caller = ModuleCaller | AnonymousUser | DomainUser;
