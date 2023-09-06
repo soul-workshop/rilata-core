bf3d6b523419fdca607432a20a7eab426400e9bd
NurGeo <anzpro@gmail.com>
2023-09-06 10:10:22 +0500

    в strict-equal-v-rule добавлен дженерик

--- src/domain/validator/rules/validate-rules/string/strict-equal.v-rule.ts
+++ src/domain/validator/rules/validate-rules/string/strict-equal.v-rule.ts
@@ -1,10 +1,10 @@
 import { ValidationRuleAnswer } from '../../types';
 import { ValidationRule } from '../../validation-rule';
 
-export class StrictEqualValidationRule extends ValidationRule<'validate', string> {
+export class StrictEqualValidationRule<S extends string> extends ValidationRule<'validate', string> {
   requirement = 'write here requirement text';
 
-  constructor(private strictString: string) {
+  constructor(private strictString: S) {
     super();
   }
 
