4d23ec2efd820996dd38d5a913b179325126fb0d
FredSteady23 <beisenov_12@mail.ru>
2023-09-12 19:19:09 +0500

    реализованы тесты range-number.v-rule

--- tests/domain/validator/rules/validate-rules/number/range-number.v-rule.test.ts
+++ tests/domain/validator/rules/validate-rules/number/range-number.v-rule.test.ts
@@ -7,4 +7,26 @@ describe('Number must be in the range', () => {
     const result = sut.validate(31);
     expect(result).toEqual({ behaviour: 'RunNextRule' });
   });
+  test('success, the resulting value is equal to the minimum value', () => {
+    const sut = new RangeNumberValidationRule(18, 64);
+    const result = sut.validate(18);
+    expect(result).toEqual({ behaviour: 'RunNextRule' });
+  });
+  test('success, the resulting value is equal to the maximum value', () => {
+    const sut = new RangeNumberValidationRule(18, 64);
+    const result = sut.validate(64);
+    expect(result).toEqual({ behaviour: 'RunNextRule' });
+  });
+  test('failure, the resulting value is greater than the maximum value', () => {
+    const sut = new RangeNumberValidationRule(18, 64);
+    const result = sut.validate(65);
+
+    expect(result).toEqual({
+      behaviour: 'SaveErrorAndRunNextRule',
+      ruleError: {
+        hint: { max: 64, min: 18 },
+        text: 'Число должно быть в диапозоне от {{min}} до {{max}}',
+      },
+    });
+  });
 });
