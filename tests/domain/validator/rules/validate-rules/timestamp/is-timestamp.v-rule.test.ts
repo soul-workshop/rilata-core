b9d3799512e54b0a667dc6dc1f5075aeebe3781b
FredSteady23 <beisenov_12@mail.ru>
2023-09-14 15:13:39 +0500

    реализован timestamp папка

---
+++ tests/domain/validator/rules/validate-rules/timestamp/is-timestamp.v-rule.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, test } from 'bun:test';
+import { IsTimeStampValidationRule } from '../../../../../../src/domain/validator/rules/validate-rules/timestamp/is-timestamp.v-rule';
+
+describe('Value is a date', () => {
+  test('success, the resulting value is a date', () => {
+    const sut = new IsTimeStampValidationRule();
+    const result = sut.validate(1694683538);
+    expect(result).toEqual({
+      behaviour: 'RunNextRule',
+    });
+  });
+  test('failure, if the value is given as a negative number', () => {
+    const sut = new IsTimeStampValidationRule();
+    const result = sut.validate(-1694683538);
+    expect(result).toEqual({
+      behaviour: 'SaveErrorAndRunNextRule',
+      ruleError: {
+        hint: {},
+        text: 'Значение не является датой',
+      },
+    });
+  });
+  test('failure, if given infinity as value', () => {
+    const sut = new IsTimeStampValidationRule();
+    const result = sut.validate(Infinity);
+    expect(result).toEqual({
+      behaviour: 'SaveErrorAndRunNextRule',
+      ruleError: {
+        hint: {},
+        text: 'Значение не является датой',
+      },
+    });
+  });
+  test('failure, if given negative infinity as value', () => {
+    const sut = new IsTimeStampValidationRule();
+    const result = sut.validate(-Infinity);
+    expect(result).toEqual({
+      behaviour: 'SaveErrorAndRunNextRule',
+      ruleError: {
+        hint: {},
+        text: 'Значение не является датой',
+      },
+    });
+  });
+  test('failure, the resulting value is Not a Number', () => {
+    const sut = new IsTimeStampValidationRule();
+    const result = sut.validate(NaN);
+    expect(result).toEqual({
+      behaviour: 'SaveErrorAndRunNextRule',
+      ruleError: {
+        hint: {},
+        text: 'Значение не является датой',
+      },
+    });
+  });
+});
