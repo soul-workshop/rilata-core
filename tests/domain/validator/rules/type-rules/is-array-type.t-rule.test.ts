9ff6c11ec31b8bfa61282ba5c47f2824623857df
xHetso <datacener23491248@gmail.com>
2023-09-15 14:44:10 +0500

    Пофиксил все ошибки

--- tests/domain/validator/rules/type-rules/is-array-type.t-rule.test.ts
+++ tests/domain/validator/rules/type-rules/is-array-type.t-rule.test.ts
@@ -1,27 +1,30 @@
-import {describe, expect,test} from "bun:test";
-import { IsArrayTypeRule } from "../../../../../src/domain/validator/rules/type-rules/is-array-type.t-rule";
+import { describe, expect, test } from 'bun:test';
+import { IsArrayTypeRule } from '../../../../../src/domain/validator/rules/type-rules/is-array-type.t-rule';
 
-describe('IsArrayTypeRule', () => {
-  test('success, reseived value equal array', () => {
+describe('IsArrayTypeRule tests', () => {
+  test('success, received value is an empty array or [1, 2, 3]', () => {
     const rule = new IsArrayTypeRule();
-    const result = rule.validate([1, 2, 3]); //отдаем массив
-    expect(result).toEqual({
-        behaviour: "RunNextRule"
+    const valuesToCheck = [[], [1, 2, 3]];
+    valuesToCheck.forEach((value) => {
+      const result = rule.validate(value);
+      expect(result).toEqual({
+        behaviour: 'RunNextRule',
+      });
     });
   });
 
   test('should return "SaveErrorAndBreakValidation" for an array with different types', () => {
     const rule = new IsArrayTypeRule();
     const valuesToCheck = ['not an array', 42, true];
-    valuesToCheck.forEach((value)=>{
-        const validationResult = rule.validate(value);
-        expect(validationResult).toEqual({
-            behaviour: "SaveErrorAndBreakValidation",
-            ruleError: {
-            "hint": {},
-            "text": "Значение должно быть массивом данных"
-            }
-        })
-    })
+    valuesToCheck.forEach((value) => {
+      const validationResult = rule.validate(value);
+      expect(validationResult).toEqual({
+        behaviour: 'SaveErrorAndBreakValidation',
+        ruleError: {
+          hint: {},
+          text: 'Значение должно быть массивом данных',
+        },
+      });
+    });
   });
 });
