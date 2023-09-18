467b6bdbc2161d468827f6d8283d22b64deca5e4
xHetso <datacener23491248@gmail.com>
2023-09-18 12:54:44 +0500

    Пофиксил все ошибки

--- tests/domain/validator/rules/type-rules/is-number-type.t-rule.test.ts
+++ tests/domain/validator/rules/type-rules/is-number-type.t-rule.test.ts
@@ -1,27 +1,27 @@
-import {describe, expect,test} from "bun:test";
-import { IsNumberTypeRule } from "../../../../../src/domain/validator/rules/type-rules/is-number-type.t-rule";
+import { describe, expect, test } from 'bun:test';
+import { IsNumberTypeRule } from '../../../../../src/domain/validator/rules/type-rules/is-number-type.t-rule';
 
 describe('IsNumberTypeRule', () => {
   test('success, received value equal number', () => {
     const rule = new IsNumberTypeRule();
     const result = rule.validate(42); // Здесь 42 - это число
     expect(result).toEqual({
-        behaviour: "RunNextRule"
+      behaviour: 'RunNextRule',
     });
   });
 
-  test('fail, received value equal not empty number', () => {
+  test('received value equal not equal number', () => {
     const rule = new IsNumberTypeRule();
     const valuesToCheck = ['not a number', ' ', '4', ['2', '3']];
     valuesToCheck.forEach((value) => {
       const validationResult = rule.validate(value);
       expect(validationResult).toEqual({
-        behaviour: "SaveErrorAndBreakValidation",
+        behaviour: 'SaveErrorAndBreakValidation',
         ruleError: {
-          "hint": {},
-          "text": "Значение должно быть числовым"
-        }
+          hint: {},
+          text: 'Значение должно быть числовым',
+        },
+      });
     });
   });
-});
 });
