dcb38bd70d00dfb282c166810098407285d824d3
Nuradil <yaksh0nti@gmail.com>
2023-11-09 15:48:18 +0500

    удален bun.lockb и добавлен в ,gitignore, некоторые зависимосты перемещены в dev зависимость в package.json, интерфейс ModuleResolver теперь наследует интерфейс Repositoriable

--- src/conf/module-resolver.ts
+++ src/conf/module-resolver.ts
@@ -1,4 +1,5 @@
+import { Repositoriable } from '../domain/repositoriable';
 import { LoggerResolver } from './logger-resolver';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ModuleResolver extends LoggerResolver {}
+export interface ModuleResolver extends LoggerResolver, Repositoriable {}
