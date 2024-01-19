fb23182b00decc807283b4f78c17038acef0bd17
xHetso <datacener23491248@gmail.com>
2024-01-19 16:59:36 +0500

    Реализовал test-resolver и test-thread-store mock

---
+++ tests/fixtures/test-thread-store-mock.ts
@@ -0,0 +1,29 @@
+import { uuidUtility } from '../../src/common/utils/uuid/uuid-utility';
+import { StorePayload, ThreadStore } from "../../src/app/async-store/types";
+import { Caller } from "../../src/app/caller";
+import { UuidType } from "../../src/common/types";
+import { TestResolverMock } from "./test-resolver-mock";
+import { storeDispatcher } from '../../src/app/async-store/store-dispatcher';
+
+const resolver = new TestResolverMock();
+
+export function getTestStoreDispatcher(actionId: UuidType, caller?: Caller): typeof storeDispatcher {
+  const storePayload: StorePayload = {
+    caller: caller ?? {
+      type: 'DomainUser',
+      userId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d8374',
+    },
+    moduleResolver: resolver,
+    actionId,
+    unitOfWorkId: uuidUtility.getNewUUID(),
+  };
+
+  storeDispatcher.setThreadStore({
+    run: () => {
+      throw new Error('Method not implemented.');
+    },
+    getStore: () => storePayload,
+  });
+
+  return storeDispatcher;
+}
