1a6d5086a1dce2d00a85b991e769c33e894a8a99
KLeBerLacky <petrop076@gmail.com>
2024-01-22 15:56:26 +0500

    промежуточный коммит

--- tests/domain/validator/rules/validate-rules/string/regex-doesnt-match-value.field-v-rule.test.ts
+++ tests/domain/validator/rules/validate-rules/string/regex-doesnt-match-value.field-v-rule.test.ts
@@ -2,35 +2,28 @@ import { describe, expect, test } from 'bun:test';
 import { RegexDoesntMatchValidationRule } from '../../../../../../src/domain/validator/rules/validate-rules/string/regex-doesnt-match-value.field-v-rule';
 
 describe('Validating strings using regular expressions', () => {
-  test('succes, ИИН is valid', () => {
-    const sut = new RegexDoesntMatchValidationRule(/^[0-9]\d{11}$/, 'Введите ИИН (12 цифр) ');
-    const result = sut.validate('0106255307');
-    expect(result).toEqual({
-      behaviour: 'SuccessRunNextRule',
-    });
-  });
-  test('failure, incorrect phone number entered', () => {
-    const sut = new RegexDoesntMatchValidationRule(/^(?!.*test)[ a-zA-Z]+$/, 'Должно присутствовать слово test');
-    const result = sut.validate('text have word test');
-    expect(result).toEqual({
-      behaviour: 'SuccessRunNextRule',
-    });
-  });
-  test('The text should not end with the word world', () => {
+  test('success, the word stairs is not present in the text', () => {
     const sut = new RegexDoesntMatchValidationRule(/stairs/, 'В тексте не должно присутствовать слово stairs');
     const result = sut.validate('there were only steps in the house');
     expect(result).toEqual({
       behaviour: 'SuccessRunNextRule',
     });
   });
-  test('The text should not end with the word world', () => {
+  test('success, the text ends with the word world', () => {
     const sut = new RegexDoesntMatchValidationRule(/world$/, 'Текст не должен заканчиваться на слово world');
     const result = sut.validate('Hello world moon');
     expect(result).toEqual({
       behaviour: 'SuccessRunNextRule',
     });
   });
-  test('The text should not end with the word world', () => {
+  test('success, text does not contain numbers', () => {
+    const sut = new RegexDoesntMatchValidationRule(/[0-9]/, 'Текст не должен содержать цифр');
+    const result = sut.validate('Hello world ten moon');
+    expect(result).toEqual({
+      behaviour: 'SuccessRunNextRule',
+    });
+  });
+  test('failure, the text ends with the word world', () => {
     const sut = new RegexDoesntMatchValidationRule(/world$/, 'Текст не должен заканчиваться на слово world');
     const result = sut.validate('Hello world');
     expect(result).toEqual({
@@ -41,7 +34,7 @@ describe('Validating strings using regular expressions', () => {
       },
     });
   });
-  test('The text should not end with the word world', () => {
+  test('failure, the word stairs is present in the text', () => {
     const sut = new RegexDoesntMatchValidationRule(/stairs/, 'В тексте не должно присутствовать слово stairs');
     const result = sut.validate('there was a stairs in the house');
     expect(result).toEqual({
@@ -52,25 +45,14 @@ describe('Validating strings using regular expressions', () => {
       },
     });
   });
-  test('failure, IIN is invalid', () => {
-    const sut = new RegexDoesntMatchValidationRule(/^[0-9]\d{11}$/, 'Введите ИИН (12 цифр) ');
-    const result = sut.validate('010625530759');
+  test('failure, text contains numbers', () => {
+    const sut = new RegexDoesntMatchValidationRule(/[0-9]/, 'Текст не должен содержать цифр');
+    const result = sut.validate('Hello world 10 moon');
     expect(result).toEqual({
       behaviour: 'SaveErrorAndRunNextRule',
       ruleError: {
         hint: {},
-        text: 'Введите ИИН (12 цифр) ',
-      },
-    });
-  });
-  test('failure, incorrect phone number entered', () => {
-    const sut = new RegexDoesntMatchValidationRule(/^(?!.*test)[ a-zA-Z]+$/, 'Должно присутствовать слово test');
-    const result = sut.validate('some text');
-    expect(result).toEqual({
-      behaviour: 'SaveErrorAndRunNextRule',
-      ruleError: {
-        hint: {},
-        text: 'Должно присутствовать слово test',
+        text: 'Текст не должен содержать цифр',
       },
     });
   });
