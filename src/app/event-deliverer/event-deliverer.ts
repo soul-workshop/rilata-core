ee7975e770d5a7e9ce9ef1b95179dc18eb2a3a3b
NurGeo <anzpro@gmail.com>
2024-02-01 22:27:03 +0500

    Добавлена реализация EventDeliverer

--- src/app/event-deliverer/event-deliverer.ts
+++ src/app/event-deliverer/event-deliverer.ts
@@ -1,3 +1,26 @@
-export class EventDeliverer {
+import { UuidType } from '../../common/types';
+import { Bus } from '../bus/bus';
+import { BusEventPublish } from '../bus/types';
+import { DomainEventRepository } from '../database/domain-event-repository';
+import { EventAsJson } from '../database/types';
+import { ModuleResolver } from '../resolves/module-resolver';
 
+export class EventDeliverer {
+  protected resolver!: ModuleResolver;
+
+  constructor(protected bus: Bus, protected eventRepo: DomainEventRepository) {}
+
+  init(resolver: ModuleResolver): void {
+    this.resolver = resolver;
+  }
+
+  async handle(eventName: string, actionId: UuidType, event: EventAsJson): Promise<void> {
+    const eventPublish: BusEventPublish = {
+      event,
+      eventName,
+      moduleName: this.resolver.getModule().moduleName,
+    };
+    await this.bus.publish(eventPublish);
+    this.eventRepo.markAsPublished(actionId);
+  }
 }
