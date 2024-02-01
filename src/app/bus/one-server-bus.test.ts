0cef8cab8f5d34d82e247caa59d6684617d777e5
NurGeo <anzpro@gmail.com>
2024-02-01 23:14:53 +0500

    Реализован OneServerBus

---
+++ src/app/bus/one-server-bus.test.ts
@@ -0,0 +1,20 @@
+import { Bus } from './bus';
+import { BusEventSubscribe, EventHandler, BusEventPublish } from './types';
+
+export class OneServerBus implements Bus {
+  protected handlers: Record<string, EventHandler> = {};
+
+  async subscribe(eventSubscribe: BusEventSubscribe, cb: EventHandler): Promise<void> {
+    this.handlers[this.getKey(eventSubscribe)] = cb;
+  }
+
+  async publish(eventPublish: BusEventPublish): Promise<void> {
+    const cb = this.handlers[this.getKey(eventPublish)];
+    if (cb === undefined) return;
+    cb(JSON.parse(eventPublish.event));
+  }
+
+  protected getKey(event: BusEventSubscribe): string {
+    return `${event.moduleName}___${event.eventName}`;
+  }
+}
